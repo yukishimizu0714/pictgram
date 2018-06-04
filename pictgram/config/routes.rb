c8246f9f1b2423da8c674fe80f785dd86d2ccfb6
清水悠貴 <shimizuyuuki@kaguwlan221164.ed.tus.ac.jp>
2018-06-04 18:14:06 +0900

    first commit

---
+++ pictgram/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'topics/new'
+
+  get 'sessions/new'
+
+  root 'pages#index'
+
+   get 'pages/help'
+   get    '/login',   to: 'sessions#new'
+   post   '/login',   to: 'sessions#create'
+   delete '/logout',  to: 'sessions#destroy'
+
+   resources 'users'
+   resources 'topics'
+end
